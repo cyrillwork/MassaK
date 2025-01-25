54e8ab6827aa32be10404b4dfa8c58e3d1dac75c
Kiselev Kirill <cyrillwork@mail.ru>
2025-01-25 13:37:53 +0300

    add build.bat

---
+++ build.bat
@@ -0,0 +1,7 @@
+ 
+C:\Qt2\5.12.2\mingw73_64\bin\qmake.exe MassaK.pro
+
+C:\Qt2\Tools\mingw730_64\bin\mingw32-make.exe -f D:/cyrill/MassaK/Makefile qmake_all
+
+C:\Qt2\Tools\mingw730_64\bin\mingw32-make.exe -j8
+
