9da4a2af7d5eeb27101bb4de0a8244d456035665
Kiselev Kirill <cyrillwork@mail.ru>
2025-03-02 18:46:47 +0300

    fix description

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
  
-C:\Qt2\5.12.2\mingw73_64\bin\qmake.exe MassaK.pro
+C:\Qt\6.8.1\mingw_64\bin\qmake.exe inspection_module.pro
 
-C:\Qt2\Tools\mingw730_64\bin\mingw32-make.exe -f D:/cyrill/MassaK/Makefile qmake_all
+C:\Qt\6.8.1\mingw_64\bin\mingw32-make.exe -f C:/cyrill/MassaK/Makefile qmake_all
 
-C:\Qt2\Tools\mingw730_64\bin\mingw32-make.exe -j8
+rem C:\Qt2\Tools\mingw730_64\bin\mingw32-make.exe -j8
 
